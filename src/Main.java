600c76cd58c2a85404228edefaff4179c207a979
Sunandan <sunandanphalke@gmail.com>
2022-11-18 21:32:36 +0530

    First attempt

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Finance finance = new Finance();
         finance.displayRecords();
+        //dsbgfbdfsdebffsvfkjsdbvgfjskdbv
     }
 }
 
